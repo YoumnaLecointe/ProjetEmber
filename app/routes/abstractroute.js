39796febd03c46004cb463ec8436540fac218073
Lecointe <lecointey@laposte.net>
2021-04-12 01:04:20 +0200

    abstract Route modif

--- app/routes/abstractroute.js
+++ app/routes/abstractroute.js
@@ -4,6 +4,8 @@ import { inject as service } from '@ember/service';
 export default class Abstractroute extends Route {
   @service userAuth;
   redirect() {
-   
+    if (!this.userAuth.isAuth) {
+      this.transitionTo('index');
+    }
   }
 }
