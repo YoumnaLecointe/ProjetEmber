63b802cf33bb49a6bfa2634b44e767b1ebfa20c9
Lecointe <lecointey@laposte.net>
2021-04-12 23:12:40 +0200

    Some mod

--- app/router.js
+++ app/router.js
@@ -5,5 +5,24 @@ export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
-
-Router.map(function () {});
+Router.map(function () {
+  this.route('index', { path: '/' });
+  this.route('board');
+  this.route('logout');
+  this.route('products', function () {
+    this.route('addproduct');
+    this.route('delete');
+  });
+  this.route('section', function () {
+    this.route('add');
+    this.route('delete', { path: 'delete/:section_id' });
+    this.route('addproduct');
+    this.route('products', { path: 'products/:section_id' }, function () {
+      this.route('edit', { path: '/edit/:section_id' });
+      this.route('addproduct', { path: 'addproduct/:section_id' });
+      this.route('delete', { path: '/delete/:product_id' });
+    });
+    this.route('edit', { path: 'edit/:section_id' });
+  });
+  this.route('myorders', function () {});
+});
